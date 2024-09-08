4c0f52d2b14c5153152937809b711df1ea9618b9
Md. Salauddin <salauddinahmed555@gmail.com>
2024-09-08 23:47:12 +0600

    changed the db with json-server

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const productsApi = createApi({
     reducerPath: "productsApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://fakestoreapi.com/",
+        baseUrl: "http://localhost:3000/",
     }),
     endpoints: (builder) => ({
         getProducts: builder.query({
