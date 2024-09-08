77fb81e2731f0cbc2f7f50883781ebc2cd14e6e7
Md. Salauddin <salauddinahmed555@gmail.com>
2024-09-08 23:03:15 +0600

    created rtk query setup and fetched product to show on the ui

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "../features/products/productsSlice";
+import { productsApi } from "../services/productsApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 
 
 export const store = configureStore({
     reducer: {
-       productsR :productSlice
+        [productsApi.reducerPath]: productsApi.reducer,
+        productsR: productSlice
     },
-})
+    middleware: (getDefaultMiddleware) => {
+       return getDefaultMiddleware().concat(productsApi.middleware)
+    }
+});
+
+setupListeners(store.dispatch);
+
+
+
--- src/features/products/ProductsView.jsx
+++ src/features/products/ProductsView.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+
+import { useGetProductsQuery } from "../../services/productsApi";
 
 const ProductsView = () => {
+    const { data: products, isLoading, error } = useGetProductsQuery();
+    const handleDeleteProduct = () => {
+        // delete product from API and update the state
+    }
     return (
         <div>
-            hello my name is khan 
+            <h2>Lsit of products</h2>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>error: {error.message}</p>}
+
+            {!isLoading && !error && products && products.length > 0 && (
+                <section className="products">
+                    {products.map((product) => {
+                        return <article key={product.id} className="product">
+                            <h3>{product.title}</h3>
+                            <p>{product.description}</p>
+                            <p>Price: {product.price}</p>
+                            <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
+                        </article>
+                    })
+
+                    }
+                </section>
+            )}
+
         </div>
     );
 };
